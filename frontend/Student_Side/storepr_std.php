07383ad7165f8b43d3de54a2fe01e4733e4a8e56
JesineWilson <jesinemw@gmail.com>
2024-10-14 21:47:15 +0530

    change

--- frontend/Student_Side/storepr_std.php
+++ frontend/Student_Side/storepr_std.php
@@ -427,7 +427,7 @@ img {
             <table>
                 <th>UG Details</th>
                 <tr>
-    <td>Branch<span style="color:red">*</span></td>
+    <td><span style="color:red">*</span></td>
     <td><select name="branch" id="branch">
                             <option value="CS">Computer Science</option>
                             <option value="COMMERCE">Commerce</option>
