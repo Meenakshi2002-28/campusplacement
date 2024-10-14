92966ec172c44e92ac3cc0ae972b4da9a496e187
Meenakshi <meenuptr2002@gmail.com>
2024-10-15 00:13:04 +0530

    ...

--- frontend/Student_Side/dashboard_std.php
+++ frontend/Student_Side/dashboard_std.php
@@ -129,19 +129,42 @@ if (isset($_SESSION['user_id'])) {
             padding-left: 5px;
             padding-right: 75px;
         }
+        .dropdown-content {
+    display: none;
+    position: absolute;
+    background-color: #2F5597;
+    min-width: 150px;
+    z-index: 1;
+    top: 55px; 
+    border-radius: 3px;
+}
+
+.dropdown-content a {
+    color: white;
+    padding: 12px 16px;
+    text-decoration: none;
+    display: block;
+}
+
+.dropdown-content a:hover {
+    background-color: #1e165f;
+    color: white;
+    border-radius: 3px;
+    }
 </style>
 </head>
 <body>
 <div class="container">
         <img src="../images/profile.png" alt="Profile Icon" class="icon" id="profileIcon" onclick="triggerFileInput()">
         <input type="file" id="fileInput" style="display: none;" accept="image/*" onchange="changeProfilePicture(event)">
+
+    <!--Dropdown Menu-->
         <i class="fas fa-caret-down fa-2x" aria-hidden="true" onclick="toggleDropdown()"></i>
         <div id="dropdownMenu" class="dropdown-content">
-            <a href="../Student_Side/profile_std.html"><i class="fa fa-fw fa-user"></i> Profile</a>
-            <a href="#logout"><i class="fas fa-power-off"></i> Log Out</a>
+            <a href="../profile_redirect.php"><i class="fa fa-fw fa-user"></i> Profile</a>
+            <a href="../logout.php"><i class="fas fa-power-off"></i> Log Out</a>
         </div>
     </div>
-
     <div class="sidebar">
         <a href="dashboard_std.php"><i class="fa fa-fw fa-home"></i> Home</a>
         <a href="jobs.php"><i class="fa fa-fw fa-search"></i> Jobs</a>
@@ -156,5 +179,37 @@ if (isset($_SESSION['user_id'])) {
 <div class="main-content">
         <h1>Welcome, <?php echo htmlspecialchars($name); ?></h1>
     </div>
+    <script>
+    // Change profile image
+        function triggerFileInput() {
+            document.getElementById('fileInput').click();
+        }
+
+        function changeProfilePicture(event) {
+            const file = event.target.files[0];
+            if (file) {
+                const reader = new FileReader();
+                reader.onload = function(e) {
+                    document.getElementById('sidebarProfilePicture').src = e.target.result; // Update the profile image in sidebar
+                    document.getElementById('profileIcon').src = e.target.result; // Update profile icon
+                };
+                reader.readAsDataURL(file); // Read the image file
+            }
+        }
+
+//Dropdown in the header area
+    let dropdownOpen = false;
+    function toggleDropdown() {
+        const dropdown = document.getElementById("dropdownMenu");
+        dropdownOpen = !dropdownOpen;
+        dropdown.style.display = dropdownOpen ? "block" : "none";
+    }
+
+    function goToProfile() {
+        showSection('personal'); // Redirect to profile section
+        toggleDropdown(); // Close the dropdown after redirection
+    }
+    </script>
+
 </body>
 </html>
