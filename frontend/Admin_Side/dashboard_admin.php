d11f6ddfde946593c6a13ed8ddf8b955c98e45d9
GowriParvathyy <gowriparvathy2004@gma.com>
2024-10-15 11:30:59 +0530

    change

--- frontend/Admin_Side/dashboard_admin.php
+++ frontend/Admin_Side/dashboard_admin.php
@@ -143,7 +143,7 @@ if (isset($_SESSION['user_id'])) {
     <a href="joblist_admin.php"><i class="fas fa-briefcase"></i> Jobs</a>
     <a href="#students"><i class="fas fa-user-graduate"></i> Students</a>
     <a href="#placements"><i class="fas fa-laptop-code"></i> Placements</a>
-    <a href="#company.html"><i class="fas fa-building"></i> Company</a>
+    <a href="company.html"><i class="fas fa-building"></i> Company</a>
     <a href="profile_admin.php"><i class="fas fa-user"></i> Profile</a>
     <a href="#feedback"><i class="fas fa-comment"></i> Feedback</a>
     <div class="logout">
