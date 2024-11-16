38fdbe2da6f48e63de1f3c3eb4bb43789882ff8c
Meenakshi <meenuptr2002@gmail.com>
2024-11-16 17:15:09 +0530

    ..

--- frontend/create_account.php
+++ frontend/create_account.php
@@ -55,7 +55,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
                     if ($stmt->execute()) {
                         // Redirect to success page
-                        header("Location: ../frontend/acc_created.html");
+                        header("Location: ../frontend/acc_pending.html");
                         exit();
                     } else {
                         echo "<script>alert('Error inserting into student table.');</script>";
