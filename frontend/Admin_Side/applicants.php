2a030515ebf3f58d8841f36ff765c373a152d729
Meenakshi <meenuptr2002@gmail.com>
2024-11-17 14:50:42 +0530

    ..

--- frontend/Admin_Side/applicants.php
+++ frontend/Admin_Side/applicants.php
@@ -504,6 +504,35 @@ $conn->close();
     ?>
     </div>
     <script>
+             function loadProfilePicture() {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'fetch_adminprofilepicture.php', true);
+        xhr.onload = function () {
+            if (xhr.status === 200) {
+                var profilePath = xhr.responseText.trim();
+                
+                document.getElementById('profileIcon').src = profilePath;
+            }
+        };
+        xhr.send();
+    }
+
+    window.onload = loadProfilePicture;
+    function showEditButton() {
+        document.getElementById('editImageButton').style.display = 'block';
+    }
+
+    function hideEditButton() {
+        document.getElementById('editImageButton').style.display = 'none';
+    }
+
+    function openModal() {
+        document.getElementById('profileModal').style.display = 'block';
+    }
+
+    function closeModal() {
+        document.getElementById('profileModal').style.display = 'none';
+    }
         // Change profile image
         function triggerFileInput() {
                 document.getElementById('fileInput').click();
